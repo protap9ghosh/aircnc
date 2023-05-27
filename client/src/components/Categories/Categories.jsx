acc474d8c6acad61d3cbf17b2813f899169be21a
Protap Ghosh <protapg518@gmail.com>
2023-05-27 13:21:43 +0600

    add categories in home page

--- client/src/components/Categories/Categories.jsx
+++ client/src/components/Categories/Categories.jsx
@@ -1,8 +1,19 @@
+import Container from "../Shared/Container/Container";
+import CategoryBox from "./CategoryBox";
+import { categories } from "./categoriesData";
 
 const Categories = () => {
    return (
       <div>
-         
+         <Container>
+            <div className="pt-4 flex flex-row items-center justify-between overflow-x-auto">
+               {
+                  categories.map(item => (
+                     <CategoryBox label={item.label} icon={item.icon} key={item.label} />
+                  ))
+               }
+            </div>
+         </Container>
       </div>
    );
 };
--- client/src/layouts/Main.jsx
+++ client/src/layouts/Main.jsx
@@ -1,9 +1,13 @@
+import { Outlet } from "react-router-dom"
 import NavBar from "../components/Shared/NavBar/NavBar"
 
 const Main = () => {
    return (
       <div>
-         <NavBar/>
+         <NavBar />
+         <div className="pt-28 pb-20">
+            <Outlet />
+         </div>
       </div>
    )
 }
