69ea3fcfeb1445f54e6caad81c48ad401e0b2135
Protap Ghosh <protapg518@gmail.com>
2023-05-27 23:38:56 +0600

    Room Details page img style

--- client/src/components/Rooms/Header.jsx
+++ client/src/components/Rooms/Header.jsx
@@ -8,8 +8,8 @@ const Header = () => {
             subtitle='Sidemen Indonesia'
          />
 
-         <div className="w-full md:h-[60vh] overflow-hidden rounded-xl">
-            <img className="object-cover w-full" src="https://a0.muscache.com/im/pictures/4f70b681-a792-4530-8c52-f2a8d262942d.jpg" alt="Header Image" />
+         <div className="w-full md:h-[70vh] overflow-hidden rounded-xl mt-5">
+            <img className="object-cover w-full h-full" src="https://a0.muscache.com/im/pictures/4f70b681-a792-4530-8c52-f2a8d262942d.jpg" alt="Header Image" />
          </div>
       </div>
    );
